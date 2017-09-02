2ad00dbd9aa785cc582df7ec7d5c6c52779bdb89
MaxXSoft <x@MaxXSoft.net>
2017-09-02 20:44:39 +0800

    (irbuilder) added some comments

--- src/irbuilder.cpp
+++ src/irbuilder.cpp
@@ -12,12 +12,13 @@ unsigned int IRBuilder::GetVariableID(SSAPtr &block, const std::string &var_name
     auto block_id = reinterpret_cast<size_t>(block.get());
     unsigned int id = 0;
 
+    // get next available id in this block
     auto var_id = block_var_id_.find(block_id);
     if (var_id == block_var_id_.end()) {
         block_var_id_.insert({block_id, 0});
     }
     else {
-        id = var_id->second++;
+        id = var_id->second++;   // increase available id
     }
 
     auto it = block_var_def_.find(block_id);
@@ -31,6 +32,7 @@ unsigned int IRBuilder::GetVariableID(SSAPtr &block, const std::string &var_name
             var_def.insert({var_name, id});
         }
         else {
+            // variable definition has already existed in current block
             id = vd_it->second;
             --var_id->second;
         }
