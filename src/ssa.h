20980b10813b555c2dd7a5ebb7b29b15efa1231f
MaxXSoft <x@MaxXSoft.net>
2017-11-25 17:25:44 +0800

    (ssa) replaced 'ssa.h' as 'ssa_new.h'

--- src/ssa.h
+++ src/ssa.h
@@ -14,26 +14,51 @@ using IDType = std::size_t;
 class BaseSSA {
 public:
     virtual ~BaseSSA() = default;
-    // virtual void Destroy() = 0;   // destroy all of the objects
+    // virtual void Generate() = 0;
+
+    void set_next(std::shared_ptr<BaseSSA> next) { next_ = next; }
+
+protected:
+    std::shared_ptr<BaseSSA> next_;
 };
 
 using SSARef = BaseSSA *;
 using SSAPtr = std::shared_ptr<BaseSSA>;
 using SSAPtrList = std::vector<SSAPtr>;
 
-class BlockSSA : public BaseSSA {
+class UseSSA : public BaseSSA {
 public:
-    BlockSSA(IDType id, SSAPtrList body) : id_(id), body_(std::move(body)) {}
+    void ReplaceBy(SSAPtr &ssa) { ref_ = ssa; }
 
-    void set_next(SSAPtr next) { next_ = next; }
-    void AddPred(SSAPtr pred) { preds_.push_back(pred); }
-    const SSAPtrList &preds() const { return preds_; }
-    IDType id() const { return id_; }
+protected:
+    UseSSA(SSAPtr ref) : ref_(ref) {}
+
+    SSAPtr ref_;
+};
+
+using UsePtr = std::shared_ptr<UseSSA>;
+
+class DefSSA : public BaseSSA {
+protected:
+    DefSSA(IDType id) : id_(id) {}
+
+    IDType id_;
+    SSAPtrList users_;
+};
+
+class PhiSSA : public BaseSSA {
+public:
+    void AddOpr(SSAPtr opr) { opr_list_.push_back(opr); }
+    void AddUser(SSAPtr user) { users_.push_back(user); }
+    void ReplaceBy(SSAPtr &ssa) {
+        // TODO
+    }
+
+    const SSAPtrList &users() const { return users_; }
+    const SSAPtrList &operands() const { return opr_list_; }
 
 private:
-    IDType id_;
-    SSAPtrList preds_, body_;
-    SSAPtr next_;
+    SSAPtrList opr_list_, users_;
 };
 
 class ValueSSA : public BaseSSA {
@@ -60,56 +85,35 @@ private:
     std::string str_val_;
 };
 
-class VariableSSA : public BaseSSA {
-public:
-    VariableSSA(IDType id) : id_(id) {}
-
-private:
-    IDType id_;
-};
-
-class PhiSSA : public BaseSSA {
-public:
-    PhiSSA(IDType block_id) : block_id_(block_id) {}
-    // PhiSSA(SSAPtrList opr_list) : opr_list_(std::move(opr_list)) {}
-    
-    void AddOpr(SSAPtr opr) { opr_list_.push_back(opr); }
-    void AddUser(SSAPtr user) { users_.push_back(user); }
-    void ReplaceBy(SSAPtr &ssa) {
-        // TODO
-    }
-
-    const SSAPtrList &users() const { return users_; }
-    const SSAPtrList &operands() const { return opr_list_; }
-    IDType block_id() const { return block_id_; }
-
-private:
-    IDType block_id_;
-    SSAPtrList opr_list_, users_;
-};
-
-class QuadSSA : public BaseSSA {
-public:
-    QuadSSA(SSAPtr dest, Operator op, SSAPtr opr1, SSAPtr opr2)
-            : dest_(dest), op_(op),
-              opr1_(opr1), opr2_(opr2) {}
-
-private:
-    SSAPtr dest_, opr1_, opr2_;
-    Operator op_;
-};
-
-class JumpSSA : public BaseSSA {
-public:
-    JumpSSA(SSAPtr block) : block_(block) {}
-
-private:
-    SSAPtr block_;
-};
-
 class UndefSSA : public BaseSSA {
 public:
     UndefSSA() {}
+};
+
+class BlockSSA : public DefSSA {
+public:
+    BlockSSA(IDType id, SSAPtr body) : DefSSA(id), body_(body) {}
+
+    IDType id() const { return id_; }
+
+private:
+    SSAPtr body_;
+    // SSAPtrList pred_;
+};
+
+class JumpSSA : public UseSSA {
+public:
+    JumpSSA(std::shared_ptr<BlockSSA> block) : UseSSA(block) {}
+};
+
+class QuadSSA : public UseSSA {
+public:
+    QuadSSA(Operator op, UsePtr opr1, UsePtr opr2)
+            : UseSSA(opr1), op_(op), opr1_(opr1), opr2_(opr2) {}
+
+private:
+    UsePtr opr1_, opr2_;
+    Operator op_;
 };
 
 #endif // SABY_SSA_H_
