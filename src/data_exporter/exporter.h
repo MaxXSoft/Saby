0cc4330395495c5d62dc7cd2130f3017e1dda8c3
MaxXSoft <x@MaxXSoft.net>
2018-02-17 20:40:11 +0800

    (exporter) completed the design of interfaces

--- src/data_exporter/exporter.h
+++ src/data_exporter/exporter.h
@@ -4,57 +4,49 @@
 #include <memory>
 #include <string>
 #include <list>
-#include <cstddef>
 
 class DataElementInterface;
 class DataReference;
 class Exporter;
 
-using UIDType = std::size_t;
-using DataGroup = std::list<std::unique_ptr<DataElementInterface>>;
-using DataRefGroup = std::list<std::unique_ptr<DataReference>>;
+using DataElement = std::unique_ptr<DataElementInterface>;
+using DataGroup = std::list<DataElement>;
+using DataRefGroup = std::list<DataReference>;
 using Exporter = std::unique_ptr<Exporter>;
 
 class DataElementInterface {
 public:
     virtual ~DataElementInterface() = default;
 
-    virtual void SetData(const std::string &key, int value) = 0;
-    virtual void SetData(const std::string &key, bool value) = 0;
-    virtual void SetData(const std::string &key, const std::string &value) = 0;
-    virtual void SetData(const std::string &key, std::nullptr_t) = 0;
-    virtual void SetData(const std::string &key, DataRefGroup &&ref) = 0;
-
-    virtual void set_uid(UIDType uid) = 0;
-    virtual void set_type(int type) = 0;
-
-    virtual UIDType uid() const = 0;
-    virtual int type() const = 0;
+    virtual void AddData(const std::string &key, long long value) = 0;
+    virtual void AddData(const std::string &key, double value) = 0;
+    virtual void AddData(const std::string &key, bool value) = 0;
+    virtual void AddData(const std::string &key, const std::string &value) = 0;
+    virtual void AddData(const std::string &key, std::nullptr_t) = 0;
+    virtual void AddData(const std::string &key, DataGroup &&value) = 0;
+    virtual void AddData(const std::string &key, DataRefGroup &&value) = 0;
 };
 
 class DataReference {
 public:
-    explicit DataReference(DataElementInterface &data) : data_(data) {}
+    explicit DataReference(DataElement &data) : data_(data) {}
 
-    DataElementInterface &data() { return data_; }
-    UIDType uid() const { return data_.uid(); }
+    DataElement &data() { return data_; }
 
 private:
-    DataElementInterface &data_;
+    DataElement &data_;
 };
 
 class ExporterInterface {
 public:
     virtual ~ExporterInterface() = default;
 
+    virtual DataElement &NewDataElement(const std::string &name) = 0;
     virtual DataGroup &NewDataGroup(const std::string &name) = 0;
     virtual DataRefGroup &NewDataRefGroup(const std::string &name) = 0;
     virtual void Export(const std::string &path) = 0;
 
     virtual void set_version(int major, int minor, int revision) = 0;
-
-protected:
-    //
 };
 
 #endif // SABY_DATA_EXPORTER_EXPORTER_H_
