56008662c3634cb959f3a9839a1534373b42f7b6
MaxXSoft <x@MaxXSoft.net>
2018-03-16 15:54:39 +0800

    (optimizer) added class 'Optimizer'

---
+++ src/optimizer.cpp
@@ -0,0 +1,313 @@
+#include "optimizer.h"
+
+#include <cmath>
+#include <limits>
+
+#include "lexer.h"
+
+namespace {
+
+const long long kNumberLimit[] = {std::numeric_limits<long long>::min(), std::numeric_limits<long long>::max()};
+const double kFloatLimit[] = {std::numeric_limits<double>::min(), std::numeric_limits<double>::max()};
+
+} // namespace
+
+template <typename T>
+T Optimizer::CalcValue(Operator op, const T &lhs, const T &rhs) {
+    switch (op) {
+        case Operator::And: return static_cast<long long>(lhs) & static_cast<long long>(rhs);
+        case Operator::Xor: return static_cast<long long>(lhs) ^ static_cast<long long>(rhs);
+        case Operator::Or: return static_cast<long long>(lhs) | static_cast<long long>(rhs);
+        case Operator::Not: return static_cast<long long>(lhs) ! static_cast<long long>(rhs);
+        case Operator::Shl: return static_cast<long long>(lhs) << static_cast<long long>(rhs);
+        case Operator::Shr: return static_cast<long long>(lhs) >> static_cast<long long>(rhs);
+        case Operator::Add: return lhs + rhs;
+        case Operator::Sub: return lhs - rhs;
+        case Operator::Mul: return lhs * rhs;
+        case Operator::Div: return lhs / rhs;
+        case Operator::Mod: return static_cast<long long>(lhs) % static_cast<long long>(rhs);
+        case Operator::Pow: return pow(lhs, rhs);
+        case Operator::Less: return lhs < rhs;
+        case Operator::LessEqual: return lhs <= rhs;
+        case Operator::Greater: return lhs > rhs;
+        case Operator::GreaterEqual: return lhs >= rhs;
+        case Operator::Equal: return lhs == rhs;
+        case Operator::NotEqual: return lhs != rhs;
+        default: return 0;
+    }
+}
+
+SSAPtr Optimizer::ConstFold(Operator op, const SSAPtr &lhs, const SSAPtr &rhs) {
+    // both of lhs or rhs must be constant
+    if (!IsSSAType<ValueSSA>(lhs) || !IsSSAType<ValueSSA>(rhs)) return nullptr;
+    SSAPtr value;
+    auto lhs_ssa = SSACast<ValueSSA>(lhs);
+    auto rhs_ssa = SSACast<ValueSSA>(rhs);
+    switch (lhs->name()[0]) {
+        case 'n': {   // #num: Number
+            auto lhs_v = lhs_ssa->num_val();
+            auto rhs_v = rhs_ssa->num_val();
+            value = std::make_shared<ValueSSA>(CalcValue<long long>(op, lhs_v, rhs_v));
+            break;
+        }
+        case 'd': {   // #dec: Decimal
+            auto lhs_v = lhs_ssa->dec_val();
+            auto rhs_v = rhs_ssa->dec_val();
+            value = std::make_shared<ValueSSA>(CalcValue<double>(op, lhs_v, rhs_v));
+            break;
+        }
+        case 's': {   // #str: String
+            auto lhs_v = lhs_ssa->str_val();
+            auto rhs_v = rhs_ssa->str_val();
+            switch (op) {
+                case Operator::Add: {   // string catenate
+                    value = std::make_shared<ValueSSA>(lhs_v + rhs_v);
+                    break;
+                }
+                case Operator::Equal: {   // str1 == str2, returns a number
+                    value = std::make_shared<ValueSSA>(static_cast<long long>(lhs_v == rhs_v));
+                    break;
+                }
+                case Operator::NotEqual: {   // str1 != str2, returns a number
+                    value = std::make_shared<ValueSSA>(static_cast<long long>(lhs_v != rhs_v));
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    return value;
+}
+
+// TODO: need to be optimized
+SSAPtr Optimizer::AlgebraSimplify(Operator op, const SSAPtr &lhs, const SSAPtr &rhs, int type) {
+    SSAPtr value = nullptr;        // non-constant value
+    ValueSSA *k_value = nullptr;   // constant value
+    bool equal, is_lhs_const;      // lhs == rhs, lhs is constant value
+    if (lhs == rhs) {
+        // handle Equal & NotEqual when lhs == rhs
+        switch (op) {
+            case Operator::Equal: return std::make_shared<ValueSSA>(1LL);
+            case Operator::NotEqual: return std::make_shared<ValueSSA>(0LL);
+            default: equal = true;
+        }
+    }
+    else {
+        switch (op) {
+            case Operator::Equal: return std::make_shared<ValueSSA>(0LL);
+            case Operator::NotEqual: return std::make_shared<ValueSSA>(1LL);
+            default: {
+                // one of operand is a constant
+                if (IsSSAType<ValueSSA>(lhs)) {
+                    k_value = SSACast<ValueSSA>(lhs);
+                    value = rhs;
+                    equal = false;
+                    is_lhs_const = true;
+                }
+                else if (IsSSAType<ValueSSA>(rhs)) {
+                    k_value = SSACast<ValueSSA>(rhs);
+                    value = lhs;
+                    equal = false;
+                    is_lhs_const = false;
+                }
+                else {
+                    // cannot do algebraic simplification
+                    return nullptr;
+                }
+            }
+        }
+    }
+    // pattern of optimizing logic expression
+    auto OptimizeLogicExpression = [&equal, &is_lhs_const, &k_value, &type](long long num_val, bool is_lhs_max) {
+        const auto lhs_index = is_lhs_max ? 1 : 0;
+        const auto rhs_index = is_lhs_max ? 0 : 1;
+        if (equal) {
+            return std::make_shared<ValueSSA>(num_val);
+        }
+        else if (is_lhs_const) {   // e.g. V_MIN <= v = 1
+            if ((type == kNumber && k_value->num_val() == kNumberLimit[lhs_index])
+                    || (type == kFloat && k_value->dec_val() == kFloatLimit[lhs_index])) {
+                return std::make_shared<ValueSSA>(num_val);
+            }
+        }
+        else {   // e.g. v <= V_MAX = 1
+            if ((type == kNumber && k_value->num_val() == kNumberLimit[rhs_index])
+                    || (type == kFloat && k_value->dec_val() == kFloatLimit[rhs_index])) {
+                return std::make_shared<ValueSSA>(num_val);
+            }
+        }
+        return std::shared_ptr<ValueSSA>(nullptr);
+    };
+    // main process of algebraic simplification
+    switch (op) {
+        case Operator::And: {   // v & v = v; v & 0 = 0; v & V_UMAX = v
+            if (equal) {
+                return value;
+            }
+            else {
+                auto k_num = k_value->num_val();
+                if (k_num == 0) {
+                    return std::make_shared<ValueSSA>(0LL);
+                }
+                else if (k_num == -1) {
+                    return value;
+                }
+            }
+            break;
+        }
+        case Operator::Xor: {   // v ^ v = 0; v ^ 0 = v
+            if (equal) {
+                return std::make_shared<ValueSSA>(0LL);
+            }
+            else if (k_value->num_val() == 0) {
+                return value;
+            }
+            break;
+        }
+        case Operator::Or: {   // v | v = v; v | 0 = v; v | V_UMAX = V_UMAX
+            if (equal) {
+                return value;
+            }
+            else {
+                auto k_num = k_value->num_val();
+                if (k_num == 0) {
+                    return value;
+                }
+                else if (k_num == -1) {
+                    return std::make_shared<ValueSSA>(-1LL);
+                }
+            }
+            break;
+        }
+        case Operator::Shl: case Operator::Shr: { // v << 0 = v; v >> 0 = v; 0 << v = 0; 0 >> v = 0
+            if (!equal && k_value->num_val() == 0) {
+                return is_lhs_const ? std::make_shared<ValueSSA>(0LL) : value;
+            }
+            break;
+        }
+        case Operator::Add: {   // v + 0 = v; v + "" = v
+            if (!equal) {
+                if (type == kNumber && k_value->num_val() == 0) return value;
+                if (type == kFloat && k_value->dec_val() == .0) return value;
+                if (type == kString && k_value->str_val().empty()) return value;
+            }
+            break;
+        }
+        case Operator::Sub: {   // v - v = 0; v - 0 = v
+            if (equal) {
+                return type == kNumber ?
+                        std::make_shared<ValueSSA>(0LL) :
+                        std::make_shared<ValueSSA>(0.);
+            }
+            else if (!is_lhs_const) {
+                if (type == kNumber && k_value->num_val() == 0) return value;
+                if (type == kFloat && k_value->dec_val() == 0.) return value;
+            }
+            break;
+        }
+        case Operator::Mul: {   // v * 0 = 0; v * 1 = v
+            if (!equal) {
+                if (type == kNumber && k_value->num_val() == 0) {
+                    return std::make_shared<ValueSSA>(0LL);
+                }
+                else if (type == kFloat && k_value->dec_val() == 0.) {
+                    return std::make_shared<ValueSSA>(0.);
+                }
+                else if ((type == kNumber && k_value->num_val() == 1)
+                        || (type == kFloat && k_value->num_val() == 1.)) {
+                    return value;
+                }
+            }
+            break;
+        }
+        case Operator::Div: {   // v / v = 1; 0 / v = 0; v / 1 = v
+            if (equal) {
+                return type == kNumber ?
+                        std::make_shared<ValueSSA>(1LL) :
+                        std::make_shared<ValueSSA>(1.);
+            }
+            else if (is_lhs_const) {   // 0 / v = 0
+                if (type == kNumber && k_value->num_val() == 0) return std::make_shared<ValueSSA>(0LL);
+                if (type == kFloat && k_value->dec_val() == 0.) return std::make_shared<ValueSSA>(0.);
+            }
+            else {   // v / 1 = v
+                if ((type == kNumber && k_value->num_val() == 1)
+                        || (type == kFloat && k_value->num_val() == 1.)) {
+                    return value;
+                }
+            }
+            break;
+        }
+        case Operator::Mod: {   // v % v = 0; 0 % v = 0; v % 1 = 0
+            if (equal) {
+                return type == kNumber ?
+                        std::make_shared<ValueSSA>(0LL) :
+                        std::make_shared<ValueSSA>(0.);
+            }
+            else if (is_lhs_const) {   // 0 % v = 0
+                if (type == kNumber && k_value->num_val() == 0) return std::make_shared<ValueSSA>(0LL);
+                if (type == kFloat && k_value->dec_val() == 0.) return std::make_shared<ValueSSA>(0.);
+            }
+            else {   // v % 1 = 0
+                if (type == kNumber && k_value->num_val() == 1) return std::make_shared<ValueSSA>(0LL);
+                if (type == kFloat && k_value->dec_val() == 1.) return std::make_shared<ValueSSA>(0.);
+            }
+            break;
+        }
+        case Operator::Pow: {   // v ** 0 = 1; 0 ** v = 0; v ** 1 = v; 1 ** v = 1
+            // NOTICE, TODO: 0 ** 0 may return 1 or 0
+            if (!equal) {
+                if (is_lhs_const) {   // 0 ** v = 0; 1 ** v = 1
+                    if (k_value->dec_val() == 0.) return std::make_shared<ValueSSA>(0.);
+                    if (k_value->dec_val() == 1.) return std::make_shared<ValueSSA>(1.);
+                }
+                else {   // v ** 0 = 1; v ** 1 = v
+                    if (k_value->dec_val() == 0.) return std::make_shared<ValueSSA>(1.);
+                    if (k_value->dec_val() == 1.) return value;
+                }
+            }
+            break;
+        }
+        case Operator::Less: {   // v < v = 0; v < V_MIN = 0; V_MAX < v = 0
+            return OptimizeLogicExpression(0, true);
+        }
+        case Operator::LessEqual: {   // v <= v = 1; v <= V_MAX = 1; V_MIN <= v = 1
+            return OptimizeLogicExpression(1, false);
+        }
+        case Operator::Greater: {   // v > v = 0; v > V_MAX = 0; V_MIN > v = 0
+            return OptimizeLogicExpression(0, false);
+        }
+        case Operator::GreaterEqual: {   // v >= v = 1; v >= V_MIN = 1; V_MAX >= v = 1
+            return OptimizeLogicExpression(1, true);
+        }
+    }
+    // cannot do optimization
+    return nullptr;
+}
+
+SSAPtr StrengthReduct(Operator op, const SSAPtr &lhs, const SSAPtr &rhs, int type) {
+    return nullptr;
+}
+
+SSAPtr Optimizer::CopyProp(const SSAPtr &rhs) {
+    if (IsSSAType<VariableSSA>(rhs)) {
+        // get the value of variable
+        auto var = SSACast<VariableSSA>(rhs);
+        const auto &value = (*var)[0].value();
+        if (IsSSAType<VariableSSA>(value)) {
+            const auto &ret = CopyProp(value);
+            return ret ? ret : value;
+        }
+        else if (IsSSAType<ValueSSA>(value) || IsSSAType<ArgGetterSSA>(value)
+                || IsSSAType<ExternFuncSSA>(value) || IsSSAType<BlockSSA>(value)) {
+            return value;
+        }
+        else {
+            return nullptr;
+        }
+    }
+    else {
+        return nullptr;
+    }
+}
+
---
+++ src/optimizer.h
@@ -0,0 +1,50 @@
+#ifndef SABY_OPTIMIZER_H_
+#define SABY_OPTIMIZER_H_
+
+/*
+    TODO: optimizer can do:
+      √ constant folding
+      √ algebraic simplification
+        strength reduction
+      √ copy propagation & constant propagation
+        common subexpression elimination
+        remove redundant jump/block
+        short-circuit evaluation (need to rewrite genir.cpp)
+        dead code elimination (using use-def or not)
+        *tail recursive optimization
+        *function inlining (optional)
+*/
+
+#include "irbuilder.h"
+
+class Optimizer {
+public:
+    using Operator = QuadSSA::Operator;
+
+    Optimizer(IRBuilder &irb) : irb_(irb), enabled_(true) {}
+    ~Optimizer() {}
+
+    SSAPtr OptimizeBinExpr(Operator op, const SSAPtr &lhs, const SSAPtr &rhs);
+    SSAPtr OptimizeUnaExpr(Operator op, const SSAPtr &operand);
+    SSAPtr OptimizeAssign(const SSAPtr &rhs);
+
+    void set_enabled(bool enabled) { enabled_ = enabled; }
+    bool enabled() const { return enabled_; }
+
+private:
+    template <typename T>
+    T CalcValue(Operator op, const T &lhs, const T &rhs);
+
+    SSAPtr ConstFold(Operator op, const SSAPtr &lhs, const SSAPtr &rhs);
+    SSAPtr AlgebraSimplify(Operator op, const SSAPtr &lhs, const SSAPtr &rhs, int type);
+    SSAPtr StrengthReduct(Operator op, const SSAPtr &lhs, const SSAPtr &rhs, int type);
+    SSAPtr CopyProp(const SSAPtr &rhs);
+    SSAPtr CSE();
+    SSAPtr RemoveRedundantJump();
+    SSAPtr DeadCodeElim();
+
+    IRBuilder &irb_;
+    bool enabled_;
+};
+
+#endif // SABY_OPTIMIZER_H_
